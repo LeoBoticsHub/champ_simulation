952d3e4b9ed3b874010dcdf7aaec978ad889ba90
GiuseppeAlfonso <giuseppealfonso92@gmail.com>
2024-09-26 16:12:42 +0000

    Added ignition launch file

---
+++ launch/gazebo_ign.launch.py
@@ -0,0 +1,131 @@
+import os
+from os import environ
+import launch_ros
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+def generate_launch_description():
+    
+    # Set the robot name for the simulation.
+    robot_name = "b1"
+    
+    # Find the package directory for "champ_simulation" using ROS 2's package index.
+    ign_pkg = launch_ros.substitutions.FindPackageShare(package="champ_simulation").find("champ_simulation")
+                                
+    # Define paths for the world file and launch directory.
+    world_file = os.path.join(ign_pkg, 'worlds', 'default_ign.world')  # Path to the default Ignition world file.
+    launch_dir = os.path.join(ign_pkg, "launch")  # Path to the launch directory of the "champ_simulation" package.
+
+    # Define the path to the Ignition configuration file.
+    # ign_config = os.path.join(
+    #     launch_ros.substitutions.FindPackageShare(package="champ_simulation").find("champ_simulation"),
+    #     "config/ignition/ign_config.yaml"
+    # )
+
+    # Declare launch arguments with default values and descriptions.
+    declare_robot_name = DeclareLaunchArgument("robot_name", default_value="go1")  # Name of the robot to be spawned.
+    declare_headless = DeclareLaunchArgument("headless", default_value="False")  # Whether to run Ignition in headless mode (without GUI).
+    declare_world_init_x = DeclareLaunchArgument("world_init_x", default_value="0.0")  # Initial x-coordinate of the robot in the world.
+    declare_world_init_y = DeclareLaunchArgument("world_init_y", default_value="0.0")  # Initial y-coordinate of the robot in the world.
+    declare_world_init_z = DeclareLaunchArgument("world_init_z", default_value="0.30")  # Initial z-coordinate of the robot in the world.
+    declare_world_init_heading = DeclareLaunchArgument("world_init_heading", default_value="0.0")  # Initial heading (yaw) of the robot.
+    declare_ign_world = DeclareLaunchArgument("ign_world", default_value=world_file, description="Ignition world name")  # Path to the Ignition world file.
+    # declare_ign_config = DeclareLaunchArgument("ign_config", default_value=ign_config, description="Ignition config file path")  # Path to the Ignition configuration file.
+    
+    # Launch configuration variables (these will be set by launch arguments).
+    robot_name = LaunchConfiguration("robot_name")
+    headless = LaunchConfiguration("headless")
+    world_init_x = LaunchConfiguration("world_init_x")
+    world_init_y = LaunchConfiguration("world_init_y")
+    world_init_z = LaunchConfiguration("world_init_z")
+    world_init_heading = LaunchConfiguration("world_init_heading")
+    ign_world = LaunchConfiguration("ign_world")
+    # ign_config = LaunchConfiguration("ign_config")
+
+    # Ignition env variables
+    env = {  # IGN GAZEBO FORTRESS env variables
+        "IGN_GAZEBO_SYSTEM_PLUGIN_PATH": ":".join(
+            [
+                environ.get("IGN_GAZEBO_SYSTEM_PLUGIN_PATH", default=""),
+                environ.get("LD_LIBRARY_PATH", default=""),
+            ]
+        ),
+    }
+    
+    # Start the Ignition client (GUI) if not running in headless mode.
+    start_ign_client_cmd = ExecuteProcess(
+        condition=IfCondition(PythonExpression([" not ", headless])),  # Only start if headless mode is not enabled.
+        cmd=["ign", "gazebo", "-v", "4", "-g"],  # Command to start the Ignition client (GUI mode).
+        cwd=[launch_dir],  # Set the working directory to the launch directory.
+        output="screen",  # Output logs to the screen.
+    )
+
+    # Start the Ignition server (simulation backend).
+    start_ign_server_cmd = ExecuteProcess(
+        cmd=[
+            "ign", "gazebo",  # Command to start the Ignition server.
+            ign_world,  # The world file to load.
+            '-r',  # Run the simulation in real-time.
+            # "--ros-args",  # Pass ROS arguments.
+            # '--params-file',  # Specify the parameters file.
+            # ign_config  # Path to the Ignition configuration file.
+        ],
+        cwd=[launch_dir],
+        
+        additional_env=env,# Set the working directory to the launch directory.
+        output="screen"  # Output logs to the screen.
+    )
+
+    # Command to spawn the robot entity in the Ignition simulation.
+    start_ign_spawner_cmd = Node(
+        package="ros_gz_sim",  # Package that provides the spawn_entity service in Ignition.
+        executable="create",  # Command to create an entity (spawn).
+        output="screen",  # Output logs to the screen.
+        arguments=[
+            "-name", robot_name,  # Name of the entity to spawn.
+            "-topic", "/robot_description",  # Topic from which to get the robot's URDF description.
+            "-x", world_init_x,  # Initial x-coordinate.
+            "-y", world_init_y,  # Initial y-coordinate.
+            "-z", world_init_z,  # Initial z-coordinate.
+            "-R", "0",  # Initial roll (fixed to 0).
+            "-P", "0",  # Initial pitch (fixed to 0).
+            "-Y", world_init_heading,  # Initial yaw (heading).
+        ]
+    )
+    
+    # ros_gz_bridge Node for ROS-Ignition bridge
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",  # Updated package name for ROS 2 and Ignition bridge
+        executable="parameter_bridge",  # Executable that provides the bridge functionality.
+        output="log",  # Output logs to the log.
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",  # Bridge between ROS 2 and Ignition Clock topics.
+            '/world/default/model/robot/joint_state@sensor_msgs/msg/JointState@ignition.msgs.Model',  # Example joint state bridge.
+            '/lidar/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+            "--ros-args",
+            "--log-level",
+            "warn",
+        ],
+        parameters=[{"use_sim_time": True}],  # Use simulated time from Ignition.
+    )
+
+    # Return the launch description, which includes all declared launch arguments and the commands to start Ignition and spawn the robot.
+    return LaunchDescription(
+        [
+            declare_headless,
+            declare_ign_world,
+            declare_robot_name,
+            declare_world_init_x,
+            declare_world_init_y,
+            declare_world_init_z,
+            declare_world_init_heading,
+            # declare_ign_config,
+            start_ign_server_cmd,
+            # start_ign_client_cmd,
+            start_ign_spawner_cmd,
+            ros_gz_bridge
+        ]
+    )
---
+++ launch/robot_ign_sim.launch.py
@@ -0,0 +1,332 @@
+import os
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def extract_base_link_from_yaml(yaml_file):
+    with open(yaml_file, 'r') as file:
+        for line in file:
+            # Look for the 'base' key and extract its value
+            if 'base' in line:
+                base_link = line.split(':')[-1].strip().strip('"')
+                return base_link
+    return None
+# Function to generate the launch description
+def generate_launch_description():
+    
+    # Chose the robot name for the simulation
+    robot_name = "b1"
+                    
+    # Package share directories
+    gazebo_pkg = launch_ros.substitutions.FindPackageShare(package="champ_simulation").find("champ_simulation")    
+    robot_description_pkg = launch_ros.substitutions.FindPackageShare(package= robot_name + "_description").find(robot_name + "_description")
+
+    # Define paths to the robot's URDF, various configuration files, the Gazebo world file, and the launch directory.
+    default_model_path = os.path.join(robot_description_pkg, "xacro/robot.xacro")
+    world_file = os.path.join(gazebo_pkg, 'worlds', 'default_ign.world')
+    launch_dir = os.path.join(gazebo_pkg, "launch")
+    
+    # Paths to specific configuration files
+    joints_config = os.path.join(robot_description_pkg, "config/joints/joints.yaml")
+    links_config = os.path.join(robot_description_pkg, "config/links/links.yaml")
+    gait_config = os.path.join(robot_description_pkg, "config/gait/gait.yaml")
+    gazebo_config = os.path.join(gazebo_pkg, "config/gazebo/gazebo.yaml")
+
+    base_link_frame = extract_base_link_from_yaml(links_config)
+
+    # Declare launch arguments for the robot description and simulation time
+    declare_description_path = DeclareLaunchArgument(
+        name="description_path", 
+        default_value=default_model_path, 
+        description="Absolute path to robot URDF file"
+    )
+    
+    declare_use_sim_time = DeclareLaunchArgument(
+        "use_sim_time", 
+        default_value="True", 
+        description="Use simulation (Gazebo) clock if true"
+    )    
+
+    # Declare launch arguments for the bringup launch file
+    declare_joints_map_path = DeclareLaunchArgument(
+        name="joints_map_path", 
+        default_value='', 
+        description="Absolute path to joints map file"
+    )
+    
+    declare_links_map_path = DeclareLaunchArgument(
+        name="links_map_path", 
+        default_value='', 
+        description="Absolute path to links map file"
+    )
+    
+    declare_gait_config_path = DeclareLaunchArgument(
+        name="gait_config_path", 
+        default_value='', 
+        description="Absolute path to gait config file"
+    )
+    
+    declare_orientation_from_imu = DeclareLaunchArgument(
+        "orientation_from_imu", 
+        default_value="false", 
+        description="Take orientation from IMU data"
+    )
+    
+    declare_robot_name = DeclareLaunchArgument(
+        "robot_name", 
+        default_value=robot_name, 
+        description="Robot name"
+    )
+    
+    declare_base_link_frame = DeclareLaunchArgument(
+        "base_link_frame", 
+        default_value=base_link_frame, 
+        description="Base link frame"
+    )
+    
+    declare_lite = DeclareLaunchArgument(
+        "lite", 
+        default_value="false", 
+        description="Lite version"
+    )
+    
+    declare_rviz = DeclareLaunchArgument(
+        "rviz", 
+        default_value="false", 
+        description="Launch RViz"
+    )
+    
+    declare_gazebo = DeclareLaunchArgument(
+        "gazebo", 
+        default_value="false", 
+        description="If in Gazebo simulation"
+    )
+    
+    declare_joint_controller_topic = DeclareLaunchArgument(
+        "joint_controller_topic", 
+        default_value="joint_group_effort_controller/joint_trajectory", 
+        description="Joint controller topic"
+    )
+    
+    declare_hardware_connected = DeclareLaunchArgument(
+        "joint_hardware_connected", 
+        default_value="false", 
+        description="Whether hardware is connected"
+    )
+    
+    declare_publish_joint_control = DeclareLaunchArgument(
+        "publish_joint_control", 
+        default_value="true", 
+        description="Publish joint control"
+    )
+    
+    declare_publish_joint_states = DeclareLaunchArgument(
+        "publish_joint_states", 
+        default_value="true", 
+        description="Publish joint states"
+    )
+    
+    declare_publish_foot_contacts = DeclareLaunchArgument(
+        "publish_foot_contacts", 
+        default_value="true", 
+        description="Publish foot contacts"
+    )
+    
+    declare_publish_odom_tf = DeclareLaunchArgument(
+        "publish_odom_tf", 
+        default_value="true", 
+        description="Publish odom tf from cmd_vel estimation"
+    )
+    
+    declare_close_loop_odom = DeclareLaunchArgument(
+        "close_loop_odom", 
+        default_value="false", 
+        description="Use closed-loop odometry"
+    )   
+
+    # Declare launch arguments for the Gazebo launch file
+    declare_headless = DeclareLaunchArgument(
+        "headless", 
+        default_value="False", 
+        description="Run Gazebo in headless mode (no GUI)"
+    )
+    
+    declare_gazebo_world = DeclareLaunchArgument(
+        "gazebo_world", 
+        default_value=world_file, 
+        description="Gazebo world name"
+    )
+    
+    declare_robot_name = DeclareLaunchArgument(
+        "robot_name", 
+        default_value=robot_name, 
+        description="Robot name"
+    )
+    
+    declare_world_init_x = DeclareLaunchArgument(
+        "world_init_x", 
+        default_value="0.0", 
+        description="Initial X position in the Gazebo world"
+    )
+    
+    declare_world_init_y = DeclareLaunchArgument(
+        "world_init_y", 
+        default_value="0.0", 
+        description="Initial Y position in the Gazebo world"
+    )
+    
+    declare_world_init_z = DeclareLaunchArgument(
+        "world_init_z", 
+        default_value="0.9", 
+        description="Initial Z position in the Gazebo world"
+    )
+    
+    declare_world_init_heading = DeclareLaunchArgument(
+        "world_init_heading", 
+        default_value="0.0", 
+        description="Initial heading (yaw) in the Gazebo world"
+    )
+    
+    declare_gazebo_config = DeclareLaunchArgument(
+        "gazebo_config", 
+        default_value=gazebo_config, 
+        description="Gazebo config file path"
+    )
+    
+    declare_launch_dir = DeclareLaunchArgument(
+        "launch_dir", 
+        default_value=launch_dir, 
+        description="Launch directory"
+    )    
+
+    declare_sensors_flag = DeclareLaunchArgument(
+        "SENSORS", 
+        default_value="false", 
+        description="Flag used to load sensors"
+    )   
+
+    declare_ign_flag = DeclareLaunchArgument(
+        "ign", 
+        default_value="true", 
+        description="Flag used to load ign control plugin"
+    ) 
+           
+    # Include the robot description launch file
+    description_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("champ_simulation"),
+                "launch",
+                "robot_description.launch.py",
+            )
+        ),
+        launch_arguments={
+            "description_path": LaunchConfiguration("description_path"),
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
+        }.items(),
+    )
+    
+    # Include the bringup launch file
+    bringup_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("champ_simulation"),
+                "launch",
+                "bringup.launch.py",
+            )
+        ),
+        launch_arguments={
+            "description_path": default_model_path,
+            "joints_map_path": joints_config,
+            "links_map_path": links_config,
+            "gait_config_path": gait_config,
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
+            "robot_name": LaunchConfiguration("robot_name"),
+            "gazebo": "true",
+            "lite": LaunchConfiguration("lite"),
+            "rviz": LaunchConfiguration("rviz"),
+            "joint_controller_topic": "joint_group_effort_controller/joint_trajectory",
+            "hardware_connected": "false",
+            "publish_foot_contacts": "false",
+            "close_loop_odom": "true",
+        }.items(),
+    )
+    
+    # Include the Gazebo launch file
+    gazebo_ign_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("champ_simulation"),
+                "launch",
+                "gazebo_ign.launch.py",
+            )
+        ),
+        launch_arguments={
+            "headless": LaunchConfiguration("headless"),
+            "gazebo_world": LaunchConfiguration("gazebo_world"),
+            "robot_name": LaunchConfiguration("robot_name"),
+            "world_init_x": LaunchConfiguration("world_init_x"),
+            "world_init_y": LaunchConfiguration("world_init_y"),
+            "world_init_z": LaunchConfiguration("world_init_z"),
+            "world_init_heading": LaunchConfiguration("world_init_heading"),
+            "SENSORS": LaunchConfiguration("SENSORS"),
+            "ign": LaunchConfiguration("ign")
+        }.items(),
+    )
+    
+    # Include the ros2_control launch file
+    ros2_control_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("champ_simulation"),
+                "launch",
+                "ros2_control.launch.py",
+            )
+        )
+    )    
+
+    # Return the LaunchDescription, including all declared launch arguments and included launch files
+    return LaunchDescription(
+        [
+            declare_description_path,
+            declare_use_sim_time,
+
+            declare_description_path,
+            declare_joints_map_path,
+            declare_links_map_path,
+            declare_gait_config_path,
+            declare_orientation_from_imu,
+            declare_robot_name,
+            declare_base_link_frame,
+            declare_lite,
+            declare_rviz,
+            declare_gazebo,
+            declare_joint_controller_topic,
+            declare_hardware_connected,
+            declare_publish_joint_control,
+            declare_publish_joint_states,
+            declare_publish_foot_contacts,
+            declare_publish_odom_tf,
+            declare_close_loop_odom,
+            
+            declare_headless,
+            declare_gazebo_world,
+            declare_robot_name,
+            declare_world_init_x,
+            declare_world_init_y,
+            declare_world_init_z,
+            declare_world_init_heading,
+            declare_sensors_flag,
+            declare_ign_flag,
+            declare_gazebo_config,
+            declare_launch_dir,
+            
+            description_ld,
+            gazebo_ign_ld,
+            ros2_control_ld,
+            bringup_ld,
+        ]
+    )
