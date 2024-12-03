0e8f968e678bec92a67f477b02ee87f5270a952d
GiuseppeAlfonso <giuseppealfonso92@gmail.com>
2024-12-03 09:43:48 +0000

    Added bag_record.launch.py

---
+++ launch/bag_record.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=[
+                'ros2', 'bag', 'record',
+                '--output', 'edoardone_bag',  # Set the name to "my_custom_bag"
+                '/imu/FL_data', '/imu/FR_data',          # Topics to record
+                '/imu/RL_data', '/imu/RR_data',
+                '/imu/trunk_data', '/joint_states',
+                '/odom/ground_truth'
+            ],
+            output='screen'
+        )
+    ])
