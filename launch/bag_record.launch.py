658bf378b00fdf8e3d523798df6d5851f77ee9b6
GiuseppeAlfonso <giuseppealfonso92@gmail.com>
2024-12-04 13:26:32 +0000

    Minor

--- launch/bag_record.launch.py
+++ launch/bag_record.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
                 '--output', 'edoardone_bag',  # Set the name to "my_custom_bag"
                 '/imu/FL_data', '/imu/FR_data',          # Topics to record
                 '/imu/RL_data', '/imu/RR_data',
-                '/imu/trunk_data', '/joint_states',
+                '/imu/trunk_data', '/joint_group_effort_controller/joint_trajectory',
                 '/odom/ground_truth'
             ],
             output='screen'
